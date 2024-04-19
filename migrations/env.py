fa5fb875c26b308ab8aa6827f0b56b0bfcea0ae7
Leonid-poc <legeorg2004@gmail.com>
2024-04-20 01:22:12 +0500

    adding metadata for feed

--- migrations/env.py
+++ migrations/env.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from src.auth.models import metadata as metadata_auth
+from src.feed.models import metadata as metadata_feed
 
 
 # this is the Alembic Config object, which provides
@@ -23,7 +24,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [metadata_auth]
+target_metadata = [
+    metadata_auth,
+    metadata_feed
+]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
