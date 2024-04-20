4f9030f59ac6d86ac59eb6f7b9481c29ae38e518
Leonid-poc <legeorg2004@gmail.com>
2024-04-21 00:52:22 +0500

    final function for get posts

--- src/feed/schemas.py
+++ src/feed/schemas.py
@@ -1,10 +1,10 @@
 from typing import List, Optional, Text
 from datetime import datetime
 from pydantic import BaseModel
-from src.users.schemas import (
-    University, Student, Department, Faculty, Group,
-    StudentRead
-)
+# from src.users.schemas import (
+#     University, Student, Department, Faculty, Group,
+#     StudentRead
+# )
 
 class EventType(BaseModel):
     id: int
@@ -15,19 +15,19 @@ class Event(BaseModel):
     id: int
     name: str
     date: datetime
-    event_type_id: EventType.id
-    university_id: University.id
+    event_type_id: int # EventType.id
+    university_id: int # University.id
 
 
 class EventRequest(BaseModel):
     id: int
     status: List[str] = ["pending", "accepted", "denied"]
-    event_id: Event.id
-    student_id: StudentRead.id
+    event_id: int # Event.id
+    student_id: int # StudentRead.id
 
 class Post(BaseModel):
     id: int
-    student_id: StudentRead.id
+    student_id: int #  StudentRead.id
     text: Text
     image: Optional[str]
     hashtags: Optional[str] = "Вуз, Волонтеры, математика"
@@ -37,35 +37,57 @@ class Post(BaseModel):
 class PostComment(BaseModel):
     id: int
     text: Text
-    student_id: StudentRead.id
-    post_id: Post.id
+    student_id: int # StudentRead.id
+    post_id: int # Post.id
 
 class PostLike(BaseModel):
     id: int
-    student_id: StudentRead.id
-    post_id: Post.id
+    student_id: int # StudentRead.id
+    post_id: int # Post.id
 
 class EventCreate(BaseModel):
-    name: Event.name
-    date: Event.date
-    event_type_id: Event.event_type_id
+    name: str # Event.name
+    date: datetime # Event.date
+    event_type_id: int # Event.event_type_id
 
 class EventRequestCreate(BaseModel):
-    event_id: EventRequest.event_id
+    event_id: int # EventRequest.event_id
 
 class PostCreate(BaseModel):
-    text: Post.text
-    image: Post.image
-    hashtags: Post.hashtags
-    event_id: Post.event_id
+    text: Text # Post.text
+    image: str # Post.image
+    hashtags: str # Post.hashtags
+    event_id: int # Post.event_id
 
 class PostRead(BaseModel):
-    city: University.city
-    university_id: Faculty.university_id
-    faculty_id: Department.faculty_id
-    department_id: Group.department_id
-    group_id: Student.group_id
-    course: Group.course
-    event_id: Post.event_id
-    event_type_id: Event.event_type_id
-    student_id: Post.student_id
+    city: str # University.city
+    university_id: int # Faculty.university_id
+    faculty_id: int # Department.faculty_id
+    department_id: int # Group.department_id
+    group_id: int # Student.group_id
+    course: int # Group.course
+    event_id: int # Post.event_id
+    event_type_id: int # Event.event_type_id
+    student_id: int # Post.student_id
+
+    def __init__(
+            self,
+            city,
+            university_id,
+            faculty_id,
+            department_id,
+            group_id,
+            course,
+            event_id,
+            event_type_id,
+            student_id
+    ) -> None:
+        self.city = city
+        self.university_id = university_id
+        self.faculty_id = faculty_id
+        self.department_id = department_id
+        self.group_id = group_id
+        self.course = course
+        self.event_id = event_id
+        self.event_type_id = event_type_id
+        self.student_id = student_id
